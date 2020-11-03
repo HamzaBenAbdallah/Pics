7fd2bde0bdd0c74a194d5d30bc3126cbb5650d4f
Hamza Ben Abdallah <hamza.ben.abdallah02@gmail.com>
2020-11-03 15:21:44 -0500

    Display the pics in a grid

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,9 +1,12 @@
+import ImageCard from './ImageCard';
+import './ImageList.css';
+
 const ImageList = props => {
-    const images = props.images.map(({ id, urls, description }) => {
-        return <img key={id} src={urls.regular} alt={description} />;
+    const images = props.images.map(image => {
+        return <ImageCard image={image} key={image.id} />;
     });
 
-    return <div>{images}</div>;
+    return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
